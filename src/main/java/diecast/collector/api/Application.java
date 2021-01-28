c0bb050eed036f75adf3cca5a3075a59e707049a
Leonardo Barbieri Bedendo <leonardobdn@gmail.com>
2021-01-28 11:47:08 -0300

    Configura swagger-ui

--- src/main/java/diecast/collector/api/Application.java
+++ src/main/java/diecast/collector/api/Application.java
@@ -1,7 +1,16 @@
 package diecast.collector.api;
 
 import io.micronaut.runtime.Micronaut;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Micronaut Rest Example PostgreSQL",
+                version = "0.1",
+                description = "Micronaut Rest API Example with PostgreSQL"
+        )
+)
 public class Application {
 
     public static void main(String[] args) {
