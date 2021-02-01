c4db8652e7cb7ed086fdd56b7aeb4d617b8a3f10
Leonardo Barbieri Bedendo <leonardobdn@gmail.com>
2021-02-01 14:51:23 -0300

    Remove construtor não utilizado

--- src/main/java/io/platosedu/domain/Brand.java
+++ src/main/java/io/platosedu/domain/Brand.java
@@ -23,10 +23,6 @@ public class Brand {
         this.id = id;
     }
 
-    public Brand(@NotNull String name) {
-        this.name = name;
-    }
-
     public Long getId() {
         return id;
     }
